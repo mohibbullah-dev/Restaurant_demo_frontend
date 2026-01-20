818c2d8cc35ab23e02eaed473b1672424eeac4bf
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-20 16:02:27 +0200

    update: all essencila files updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Menu from "./pages/Menu.jsx";
+import Checkout from "./pages/Checkout.jsx";
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+}
