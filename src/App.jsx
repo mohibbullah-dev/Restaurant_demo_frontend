f77d91a6a3ceeada3447763e0f762516cc3a1025
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-20 23:27:32 +0200

    modi: app.jsx file is modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Home from "./pages/Home.jsx";
 import Menu from "./pages/Menu.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import AdminOrders from "./pages/AdminOrders.jsx";
+
+import AdminLogin from "./pages/AdminLogin.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import AdminMenu from "./pages/AdminMenu.jsx";
+import AdminSettings from "./pages/AdminSettings.jsx";
+// import AdminOrders from "./pages/AdminOrders.jsx";
+import AdminGuard from "./components/AdminGuard.jsx";
+
 export default function App() {
   return (
     <Routes>
@@ -12,6 +20,43 @@ export default function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/admin/orders" element={<AdminOrders />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+
+        <Route
+          path="/admin"
+          element={
+            <AdminGuard>
+              <AdminDashboard />
+            </AdminGuard>
+          }
+        />
+
+        <Route
+          path="/admin/menu"
+          element={
+            <AdminGuard>
+              <AdminMenu />
+            </AdminGuard>
+          }
+        />
+
+        <Route
+          path="/admin/settings"
+          element={
+            <AdminGuard>
+              <AdminSettings />
+            </AdminGuard>
+          }
+        />
+
+        <Route
+          path="/admin/orders"
+          element={
+            <AdminGuard>
+              <AdminOrders />
+            </AdminGuard>
+          }
+        />
       </Route>
     </Routes>
   );
