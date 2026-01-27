658aa571c336d94027e563fe8ddcf1c874efc18f
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-27 22:18:23 +0200

    fix: hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -149,7 +149,7 @@ export const Hero = () => {
   // bg-obsidian
   return (
     // Added overflow-x-hidden to prevent the "white stripe" bug
-    <section className="relative min-h-screen flex flex-col justify-center overflow-x-hidden py-10 lg:py-0">
+    <section className="relative h-screen flex flex-col justify-center overflow-x-hidden py-0 lg:py-0">
       {/* 1. CLOSED NOTICE: Adjusted for mobile spacing */}
       {!settings?.isOpen && (
         <div className="absolute top-24 lg:top-32 left-1/2 -translate-x-1/2 z-50 w-full px-4 max-w-xl">
